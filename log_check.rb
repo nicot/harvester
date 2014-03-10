febf313531f8c9754a68342da1dc31be4d5d455e
Nico Tonozzi <dominic.tonozzi@gmail.com>
2014-03-10 15:10:27 -0600

    Prettified

--- log_check.rb
+++ log_check.rb
@@ -47,11 +47,6 @@ end
 $logConfigs.each do |file, config|
 	copy_file = file + ".copy"
 	
-
-	##############################################
-	#### Read in the new part of the log file
-
-	# Check if the log file exists
 	if !File.exist?(file) 
 		if debug_level > 0
 			puts "The log file "+file+" does not exist."
@@ -73,35 +68,27 @@ $logConfigs.each do |file, config|
 	if (File.exist?(copy_file))
 		# Read the first line from the copy file
 		copyfile_firstline = File.open(file, 'r') { |handle| handle.readline()}
-
 		# compare the first lines from each file. 
 		if (log_firstline != copyfile_firstline)
 			# If they differ, the log has been rotated since the last run.
-
 			# So, we should blank out the copy
 			File.open(copy_file, 'w') { |handle| handle.write("")}
 			# This will prevent the copy of the log growing infinitely long
 		end
-		# Now we need to "diff" the files and store the result into a variable
 		# iow: only grab the new log messages (since the last time this script ran)
-		
 		# An easy way to do this is to count the number of lines in the copy file, 
 		#  and take that many lines off the top of the real file, then use the rest
 		copyfile_linecount = %x{wc -l #{copy_file}}.split.first.to_i
 		logfile_contents = `tail -n +#{copyfile_linecount+1} #{file}`
-
 	# Else
 	else
 		# Read in the whole thing to a variable (same variable as above)
-		logfile_contents = `cat #{file}`
+		logfile_contents = File.open(file, 'r') { |handle| handle.read()}
 	end
-	##############################################
-	##############################################
-	#### Matcher/Responder logic
 
+	#### Matcher/Responder logic
 	# Get all the matchers
 	allMatchers = config.map{|x| x[:matchers]}.flatten.uniq
-
 	# If we want to do a filter behavior, add CatchAllMatcher
 	if config.map{|x| x[:behavior]}.include? :filter
 		allMatchers.push(CatchAllMatcher.new)
@@ -119,16 +106,13 @@ $logConfigs.each do |file, config|
 		allMatches[matcher.class.name] = result
 	end
 
-
 	# Pass the appropriate subset of matches into each responder
 	config.each do |mrSet|
 		matcherNames = mrSet[:matchers].map{|matcher| matcher.class.name}
-		
 		matcherMatches = MatchSet.new(allMatches
 			.select{|name,matches| matcherNames.include? name}
 			.values
 			.map{|matchset| matchset.matches}.flatten)
-		
 		if mrSet[:behavior] == :filter
 			# If we want to filter, basically do an array subtraction
 			# (<everything> - <matches>)
@@ -136,21 +120,13 @@ $logConfigs.each do |file, config|
 		else
 			relevantMatches = matcherMatches
 		end
-
 		responders = mrSet[:responders]
 		responders.each do |responder|
 			responder.respond(relevantMatches)
 		end
 	end
 
-	### Check the variable against each of the appropriate matchers (as defined in the config)
-	#matchesHash = Utils.findMatches(logfile_contents, matchers)
-	#Utils.processMatches(matchesHash, responders, LogCheckConfig)
-	##############################################
-	##############################################
-	#### Write out differences to the copy file
 	# This is commented out for development, so that we don't
 	#  have to empty the copy logfile every time
 	#File.open(copy_file, 'a') { |handle| handle.write(logfile_contents)}
-	##############################################
 end
